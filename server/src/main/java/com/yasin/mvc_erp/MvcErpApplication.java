47592c55d618078ef1168cc8efe23615de4fcca6
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-10-24 18:11:27 +0200

    init project

---
+++ server/src/main/java/com/yasin/mvc_erp/MvcErpApplication.java
@@ -0,0 +1,13 @@
+package com.yasin.mvc_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcErpApplication.class, args);
+	}
+
+}
